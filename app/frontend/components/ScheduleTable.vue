5ac83e3d8eec1019077c844cbbf30ee25dc68864
Rogelio Alvarado Vilchis <rogalvil@gmail.com>
2024-07-30 23:22:58 -0600

    Remove code unused

--- app/frontend/components/ScheduleTable.vue
+++ app/frontend/components/ScheduleTable.vue
@@ -12,19 +12,6 @@
     <template v-for="day in days" :key="day" v-slot:[`item.${day}`]="{ value }">
       <v-chip :color="getEngineerColor(value)"> {{ value }} </v-chip>
     </template>
-    <!-- <template v-slot:item="{ item }">
-      <tr>
-        <td>{{ item.hour }}:00 - {{ item.hour +1 }}:00</td>
-        <td v-for="day in days" :key="day">
-          <v-chip
-            v-if="item.schedule[day]"
-            :color="getEngineerColor(item.schedule[day].engineer)"
-          >
-            {{ item.schedule[day].engineer || '⚠' }}
-          </v-chip>
-        </td>
-      </tr>
-    </template> -->
   </v-data-table>
 </template>
 
@@ -60,13 +47,6 @@ export default {
         return entry;
       });
     }
-    // formattedSchedule() {
-    //   const hours = Array.from(new Set(Object.values(this.schedule).flatMap(day => Object.keys(day).map(Number)))).sort((a, b) => a - b);
-    //   return hours.map(hour => ({
-    //     hour,
-    //     schedule: Object.fromEntries(this.days.map(day => [day, this.schedule[day][hour]]))
-    //   }));
-    // }
   },
   methods: {
     getEngineerColor(engineer) {
@@ -84,73 +64,3 @@ export default {
 
 <style scoped>
 </style>
-
-<!-- <template>
-  <v-data-table
-    :headers="headers"
-    :items="formattedSchedule"
-    :items-per-page="hours.length"
-    hide-default-footer
-    density="compact"
-  >
-    <template v-slot:item="{ item }">
-      <tr>
-        <td>{{ item.hour }}</td>
-        <td v-for="day in days" :key="day">
-          <v-chip
-            v-if="item.schedule[day].engineer"
-            :color="getEngineerColor(item.schedule[day].engineer)"
-            class="ma-2"
-          >
-            {{ item.schedule[day].engineer || '⚠' }}
-          </v-chip>
-        </td>
-      </tr>
-    </template>
-  </v-data-table>
-</template>
-
-<script>
-export default {
-  props: {
-    schedule: {
-      type: Object,
-      required: true
-    }
-  },
-  computed: {
-    days() {
-      return Object.keys(this.schedule);
-    },
-    hours() {
-      return Object.keys(this.schedule[this.days[0]]);
-    },
-    headers() {
-      return [
-        { title: 'Hora', align: 'start', sortable: false, key: 'hour' },
-        ...this.days.map(day => ({ title: day, align: 'start', sortable: false, key: day }))
-      ];
-    },
-    formattedSchedule() {
-      return this.hours.map(hour => ({
-        hour,
-        schedule: Object.fromEntries(this.days.map(day => [day, this.schedule[day][hour] || {}]))
-      }));
-    }
-  },
-  methods: {
-    getEngineerColor(engineer) {
-      // if (engineer === '⚠') return 'red';
-      const engineerColors = {
-        Ernesto: 'green',
-        Bárbara: 'purple',
-        Benjamín: 'blue'
-      };
-      return engineerColors[engineer] || 'gray';
-    }
-  }
-};
-</script>
-
-<style scoped>
-</style> -->
