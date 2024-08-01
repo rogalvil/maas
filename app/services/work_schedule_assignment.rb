f58384ea1ada14e4a82a32ca4da39a63c1cd3f66
Rogelio Alvarado Vilchis <rogalvil@gmail.com>
2024-08-01 16:50:56 -0600

    Refactor to assign consecutive hours

--- app/services/work_schedule_assignment.rb
+++ app/services/work_schedule_assignment.rb
@@ -34,18 +34,41 @@ class WorkScheduleAssignment
     hours = (schedule.start_time...schedule.end_time).to_a
     available_engineers = available_engineers_for_day(day)
 
-    available_engineers.each do |engineer|
-      assign_consecutive_hours(day, hours, engineer)
+    assign_least_hours_engineers(day, hours, available_engineers, 2)
+    assign_remaining_hours(day, hours, available_engineers)
+  end
+
+  def assign_least_hours_engineers(day, hours, available_engineers, minimum_engineers)
+    minimum_engineers.times do
+      engineer = find_least_assigned_engineer(available_engineers, day, hours)
+      assign_consecutive_hours(day, hours, engineer) if engineer
+      available_engineers.delete(engineer)
+    end
+  end
+
+  def assign_remaining_hours(day, hours, available_engineers)
+    while hours.any? && available_engineers.any?
+      engineer = find_least_assigned_engineer(available_engineers, day, hours)
+      assign_consecutive_hours(day, hours, engineer) if engineer
+      available_engineers.delete(engineer)
     end
   end
 
   def assign_consecutive_hours(day, hours, engineer)
+    assigned_hours = []
     hours.each do |hour|
       next if already_assigned?(day, hour)
+      next unless available?(engineer, day, hour)
 
       @assign_work_schedules << create_work_schedule(day, hour, engineer)
+      assigned_hours << hour
     end
+    hours.reject! { |hour| assigned_hours.include?(hour) }
   end
+
+  # def available_engineers_for(day, hour)
+  #   @engineer_availabilities.select { |availability| availability.day_of_week == day_number(day) && availability.hour == hour }
+  # end
 
   def available_engineers_for_day(day)
     @engineer_availabilities.select { |availability| availability.day_of_week == day_number(day) }.map(&:engineer).uniq
@@ -53,6 +76,41 @@ class WorkScheduleAssignment
 
   def already_assigned?(day, hour)
     @assign_work_schedules.any? { |schedule| schedule[:day_of_week] == day_number(day) && schedule[:hour] == hour }
+  end
+
+  def find_least_assigned_engineer(engineers, day, hours)
+    engineer_hours = engineers.map do |engineer|
+      [engineer, count_assigned_hours(engineer, day, hours)]
+    end.to_h
+
+    engineer_hours.min_by { |_engineer, h| h }&.first
+  end
+
+  # def count_assigned_hours(engineer, day, hours)
+  #   assigned_hours = @assign_work_schedules.count { |schedule| schedule[:engineer_id] == engineer.id }
+  #   available_hours = hours.count { |hour| available?(engineer, day, hour) }
+  #   assigned_hours + (24 - available_hours)
+  # end
+  # def count_assigned_hours(engineer, day, hours)
+  #   max_hours = 6
+  #   assigned_hours = @assign_work_schedules.count { |schedule| schedule[:engineer_id] == engineer.id }
+  #   available_hours = hours.count { |hour| available?(engineer, day, hour) }
+  #   assigned_hours + (max_hours - available_hours)
+  # end
+  def count_assigned_hours(engineer, day, hours)
+    max_hours = 8
+    assigned_hours = @assign_work_schedules.count { |schedule| schedule[:engineer_id] == engineer.id }
+    available_hours = hours.count { |hour| available?(engineer, day, hour) }
+    penalty = [0, max_hours - available_hours].max
+    assigned_hours + penalty
+  end
+
+  def available?(engineer, day, hour)
+    @engineer_availabilities.any? do |availability|
+      availability.engineer_id == engineer.id &&
+        availability.day_of_week == day_number(day) &&
+        availability.hour == hour
+    end
   end
 
   def create_work_schedule(day, hour, engineer)
